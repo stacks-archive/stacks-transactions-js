46a38a846678b190ca4288484c26c6f2e550b0a6
Reed Rosenbluth <reed.rosenbluth@gmail.com>
2020-05-06 12:44:55 -0600

    refactor: move lexicographical ordering of tuple data from serialize to constructor

--- src/clarity/types/tupleCV.ts
+++ src/clarity/types/tupleCV.ts
@@ -1,18 +1,32 @@
 import { ClarityType, ClarityValue } from '../clarityValue';
 import { isClarityName } from '../../utils';
 
+type TupleData = { [key: string]: ClarityValue };
+
 interface TupleCV {
   type: ClarityType.Tuple;
-  data: { [key: string]: ClarityValue };
+  data: TupleData;
 }
 
-function tupleCV(data: { [key: string]: ClarityValue }): TupleCV {
+function tupleCV(data: TupleData): TupleCV {
   for (const key in data) {
     if (!isClarityName(key)) {
       throw new Error(`"${key}" is not a valid Clarity name`);
     }
   }
-  return { type: ClarityType.Tuple, data };
+
+  const ordered: TupleData = {};
+  Object.keys(data)
+    .sort((a, b) => {
+      const bufA = Buffer.from(a);
+      const bufB = Buffer.from(b);
+      return bufA.compare(bufB);
+    })
+    .forEach(key => {
+      ordered[key] = data[key];
+    });
+
+  return { type: ClarityType.Tuple, data: ordered };
 }
 
 export { TupleCV, tupleCV };
