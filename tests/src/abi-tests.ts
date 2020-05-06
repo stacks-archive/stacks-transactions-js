da86045e1a330c541a6402cefc3fbc96367baaa3
Reed Rosenbluth <reed.rosenbluth@gmail.com>
2020-05-06 15:51:02 -0600

    test: increase coverage

--- tests/src/abi-tests.ts
+++ tests/src/abi-tests.ts
@@ -10,6 +10,10 @@ import {
   someCV,
   falseCV,
   listCV,
+  responseOkCV,
+  responseErrorCV,
+  noneCV,
+  ClarityValue,
 } from '../../src';
 import { validateContractCall, ClarityAbi } from '../../src/contract-abi';
 
@@ -29,7 +33,8 @@ test('ABI validation', () => {
       key4: standardPrincipalCV('SP2JXKMSH007NPYAQHKJPQMAQYAD90NQGTVJVQ02B'),
       key5: bufferCVFromString('foo'),
       key6: someCV(falseCV()),
-      key7: listCV([trueCV(), falseCV()]),
+      key7: responseOkCV(trueCV()),
+      key8: listCV([trueCV(), falseCV()]),
     }),
   ];
 
@@ -43,19 +48,20 @@ test('ABI validation', () => {
   validateContractCall(payload, TEST_ABI);
 });
 
-test('ABI validation fail', () => {
+test('ABI validation fail, tuple mistyped', () => {
   const contractAddress = 'ST3KC0MTNW34S1ZXD36JYKFD3JJMWA01M55DSJ4JE';
   const contractName = 'test';
   const functionName = 'tuple-test';
   const functionArgs = [
     tupleCV({
-      key1: intCV(1),
+      key1: trueCV(),
       key2: intCV(-1),
       key3: uintCV(1),
       key4: standardPrincipalCV('SP2JXKMSH007NPYAQHKJPQMAQYAD90NQGTVJVQ02B'),
       key5: bufferCVFromString('foo'),
-      key6: someCV(falseCV()),
-      key7: listCV([trueCV(), falseCV()]),
+      key6: noneCV(),
+      key7: responseErrorCV(trueCV()),
+      key8: falseCV(),
     }),
   ];
 
@@ -67,6 +73,53 @@ test('ABI validation fail', () => {
   );
 
   expect(() => validateContractCall(payload, TEST_ABI)).toThrow(
-    'Clarity function `tuple-test` expects argument 0 to be of type tuple("key1":bool,"key2":int128,"key3":uint128,"key4":principal,"key5":buffer(3),"key6":optional(bool),"key7":list(bool,2)), not tuple("key1":int128,"key2":int128,"key3":uint128,"key4":principal,"key5":buffer(3),"key6":optional(bool),"key7":list(bool,2))'
+    'Clarity function `tuple-test` expects argument 0 to be of type tuple("key1":bool,"key2":int128,"key3":uint128,"key4":principal,"key5":buffer(3),"key6":optional(bool),"key7":response(bool,bool),"key8":list(bool,2))'
+  );
+});
+
+test('ABI validation fail, tuple wrong key', () => {
+  const contractAddress = 'ST3KC0MTNW34S1ZXD36JYKFD3JJMWA01M55DSJ4JE';
+  const contractName = 'kv-store';
+  const functionName = 'tuple-test';
+  const functionArgs = [
+    tupleCV({
+      'wrong-key': trueCV(),
+      key2: intCV(-1),
+      key3: uintCV(1),
+      key4: standardPrincipalCV('SP2JXKMSH007NPYAQHKJPQMAQYAD90NQGTVJVQ02B'),
+      key5: bufferCVFromString('foo'),
+      key6: someCV(falseCV()),
+      key7: responseOkCV(trueCV()),
+      key9: listCV([trueCV(), falseCV()]),
+    }),
+  ];
+
+  const payload = createContractCallPayload(
+    contractAddress,
+    contractName,
+    functionName,
+    functionArgs
+  );
+
+  expect(() => validateContractCall(payload, TEST_ABI)).toThrow(
+    'Clarity function `tuple-test` expects argument 0 to be of type tuple("key1":bool,"key2":int128,"key3":uint128,"key4":principal,"key5":buffer(3),"key6":optional(bool),"key7":response(bool,bool),"key8":list(bool,2))'
+  );
+});
+
+test('ABI validation fail, wrong number of args', () => {
+  const contractAddress = 'ST3KC0MTNW34S1ZXD36JYKFD3JJMWA01M55DSJ4JE';
+  const contractName = 'kv-store';
+  const functionName = 'tuple-test';
+  const functionArgs = [trueCV(), falseCV()];
+
+  const payload = createContractCallPayload(
+    contractAddress,
+    contractName,
+    functionName,
+    functionArgs
+  );
+
+  expect(() => validateContractCall(payload, TEST_ABI)).toThrow(
+    'Clarity function expects 1 argument(s) but received 2'
   );
 });
