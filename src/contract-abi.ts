b7663d87d8f00fc47ff91e344d68971e580cdd4b
Thomas Osmonson <thomas.osmonson@gmail.com>
2020-05-21 10:38:39 -0500

    fix: run lint:prettier:fix

--- src/contract-abi.ts
+++ src/contract-abi.ts
@@ -343,8 +343,9 @@ export function validateContractCall(payload: ContractCallPayload, abi: ClarityA
 
       if (!matchType(payloadArg, abiArg.type)) {
         throw new Error(
-          `Clarity function \`${payload.functionName.content}\` expects argument ${i +
-            1} to be of type ${getTypeString(abiArg.type)}, not ${getCVTypeString(payloadArg)}`
+          `Clarity function \`${payload.functionName.content}\` expects argument ${
+            i + 1
+          } to be of type ${getTypeString(abiArg.type)}, not ${getCVTypeString(payloadArg)}`
         );
       }
     }
