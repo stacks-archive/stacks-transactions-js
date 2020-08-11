3ebe08338fecf4ecf8da92c464e08b4bf667da47
Reed Rosenbluth <reed.rosenbluth@gmail.com>
2020-08-11 14:08:31 -0400

    test: multisig auth and tx signing

--- tests/src/authorization-tests.ts
+++ tests/src/authorization-tests.ts
@@ -1,7 +1,13 @@
 import {
-  SpendingCondition,
   SingleSigSpendingCondition,
   MessageSignature,
+  createSingleSigSpendingCondition,
+  serializeSpendingCondition,
+  deserializeSpendingCondition,
+  createMessageSignature,
+  emptyMessageSignature,
+  createMultiSigSpendingCondition,
+  createTransactionAuthField,
 } from '../../src/authorization';
 
 import { addressFromVersionHash } from '../../src/types';
@@ -10,7 +16,14 @@ import { AddressHashMode, AddressVersion, PubKeyEncoding } from '../../src/const
 
 import * as BigNum from 'bn.js';
 import { BufferReader } from '../../src/bufferReader';
-import { createStacksPrivateKey, signWithKey } from '../../src/keys';
+import {
+  createStacksPrivateKey,
+  signWithKey,
+  pubKeyfromPrivKey,
+  publicKeyToString,
+  createStacksPublicKey,
+} from '../../src/keys';
+import { signature } from 'bitcoinjs-lib/types/script';
 
 test('ECDSA recoverable signature', () => {
   const privKeyString = 'edf9aee84d9b7abc145504dde6726c64f369d37ee34ded868fabd876c26570bc';
@@ -19,8 +32,8 @@ test('ECDSA recoverable signature', () => {
     '019901d8b1d67a7b853dc473d0609508ab2519ec370eabfef460aa0fd9234660' +
     '787970968562da9de8b024a7f36f946b2fdcbf39b2f59247267a9d72730f19276b';
   const privKey = createStacksPrivateKey(privKeyString);
-  const signature = signWithKey(privKey, messagetoSign).toString();
-  expect(signature).toBe(correctSignature);
+  const messageSignature = signWithKey(privKey, messagetoSign);
+  expect(messageSignature.signature).toBe(correctSignature);
 });
 
 test('Single spending condition serialization and deserialization', () => {
@@ -29,12 +42,14 @@ test('Single spending condition serialization and deserialization', () => {
   const fee = new BigNum(0);
   const pubKey = '03ef788b3830c00abe8f64f62dc32fc863bc0b2cafeb073b6c8e1c7657d9c2c3ab';
   const secretKey = 'edf9aee84d9b7abc145504dde6726c64f369d37ee34ded868fabd876c26570bc01';
-  const spendingCondition = new SingleSigSpendingCondition(addressHashMode, pubKey, nonce, fee);
-  const emptySignature = MessageSignature.empty();
+  const spendingCondition = createSingleSigSpendingCondition(addressHashMode, pubKey, nonce, fee);
+  const emptySignature = emptyMessageSignature();
 
-  const serialized = spendingCondition.serialize();
-  const deserialized = SpendingCondition.deserialize(new BufferReader(serialized));
-  expect(deserialized.addressHashMode).toBe(addressHashMode);
+  const serialized = serializeSpendingCondition(spendingCondition);
+  const deserialized = deserializeSpendingCondition(
+    new BufferReader(serialized)
+  ) as SingleSigSpendingCondition;
+  expect(deserialized.hashMode).toBe(addressHashMode);
   expect(deserialized.nonce!.toNumber()).toBe(nonce.toNumber());
   expect(deserialized.fee!.toNumber()).toBe(fee.toNumber());
   expect(deserialized.signature.toString()).toBe(emptySignature.toString());
@@ -45,17 +60,14 @@ test('Single sig spending condition uncompressed', () => {
   const nonce = new BigNum(123);
   const fee = new BigNum(456);
   const pubKey = '';
-  const spendingCondition = new SingleSigSpendingCondition(addressHashMode, pubKey, nonce, fee);
-  spendingCondition.signerAddress = addressFromVersionHash(
-    AddressVersion.MainnetSingleSig,
-    '11'.repeat(20)
-  );
-  spendingCondition.pubKeyEncoding = PubKeyEncoding.Uncompressed;
+  const spendingCondition = createSingleSigSpendingCondition(addressHashMode, pubKey, nonce, fee);
+  spendingCondition.signer = '11'.repeat(20);
+  spendingCondition.keyEncoding = PubKeyEncoding.Uncompressed;
 
-  const signature = new MessageSignature('ff'.repeat(65));
+  const signature = createMessageSignature('ff'.repeat(65));
   spendingCondition.signature = signature;
 
-  const serializedSpendingCondition = spendingCondition.serialize();
+  const serializedSpendingCondition = serializeSpendingCondition(spendingCondition);
 
   // prettier-ignore
   const spendingConditionBytesHex = [
@@ -82,43 +94,56 @@ test('Single sig spending condition uncompressed', () => {
   expect(serializedSpendingCondition).toEqual(spendingConditionBytes);
 });
 
-test('Single sig wpkh spending condition compressed', () => {
-  const addressHashMode = AddressHashMode.SerializeP2WPKH;
-  const nonce = new BigNum(345);
+test('Multi sig spending condition uncompressed', () => {
+  const addressHashMode = AddressHashMode.SerializeP2SH;
+  const nonce = new BigNum(123);
   const fee = new BigNum(456);
-  const pubKey = '';
-  const spendingCondition = new SingleSigSpendingCondition(addressHashMode, pubKey, nonce, fee);
-  spendingCondition.signerAddress = addressFromVersionHash(
-    AddressVersion.MainnetSingleSig,
-    '11'.repeat(20)
+  const pubKey = '02'.repeat(33);
+  const pubKeys = [pubKey, pubKey, pubKey];
+
+  const spendingCondition = createMultiSigSpendingCondition(
+    addressHashMode,
+    2,
+    pubKeys,
+    nonce,
+    fee
   );
-  spendingCondition.pubKeyEncoding = PubKeyEncoding.Compressed;
+  spendingCondition.signer = '11'.repeat(20);
 
-  const signature = new MessageSignature('fe'.repeat(65));
-  spendingCondition.signature = signature;
+  const signature = createMessageSignature('ff'.repeat(65));
+  const fields = [signature, signature, createStacksPublicKey(pubKeys[2])];
+  spendingCondition.fields = fields.map(createTransactionAuthField);
 
-  const serializedSpendingCondition = spendingCondition.serialize();
+  const serializedSpendingCondition = serializeSpendingCondition(spendingCondition);
 
   // prettier-ignore
   const spendingConditionBytesHex = [
     // address hash mode
-    AddressHashMode.SerializeP2WPKH,
+    AddressHashMode.SerializeP2SH,
     // signer
     0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 
     0x11, 0x11, 0x11, 0x11, 0x11,
     // nonce
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x59,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x7b,
     // fee rate
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0xc8,
-    // key encoding
-    PubKeyEncoding.Compressed,
-    // signature
-    0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 
-    0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 
-    0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 
-    0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe, 0xfe,
-    0xfe, 0xfe, 0xfe, 0xfe, 0xfe
+    // length prefixed list of fields
+    0x00, 0x00, 0x00, 0x03,
+    0x02, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff,
+    0x02, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff,
+    0x00, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02,
+    // required signatures
+    0x00, 0x02
   ];
+
   const spendingConditionBytes = Buffer.from(spendingConditionBytesHex);
 
   expect(serializedSpendingCondition).toEqual(spendingConditionBytes);
--- tests/src/transaction-tests.ts
+++ tests/src/transaction-tests.ts
@@ -1,12 +1,26 @@
 import { StacksTransaction, deserializeTransaction } from '../../src/transaction';
 
-import { StandardAuthorization, SingleSigSpendingCondition } from '../../src/authorization';
+import {
+  StandardAuthorization,
+  createSingleSigSpendingCondition,
+  SingleSigSpendingCondition,
+  createMultiSigSpendingCondition,
+  MultiSigSpendingCondition,
+  Authorization,
+  serializeSpendingCondition,
+  deserializeSpendingCondition,
+} from '../../src/authorization';
 
 import { TokenTransferPayload, createTokenTransferPayload } from '../../src/payload';
 
 import { STXPostCondition, createSTXPostCondition } from '../../src/postcondition';
 
-import { createLPList, createStandardPrincipal } from '../../src/types';
+import {
+  createLPList,
+  createStandardPrincipal,
+  serializeLPList,
+  deserializeLPList,
+} from '../../src/types';
 
 import {
   DEFAULT_CORE_NODE_API_URL,
@@ -17,11 +31,20 @@ import {
   AuthType,
   FungibleConditionCode,
   AddressHashMode,
+  AddressVersion,
+  StacksMessageType,
 } from '../../src/constants';
 
-import { hash_p2pkh } from '../../src/utils';
+import { hashP2PKH } from '../../src/utils';
 
-import { createStacksPrivateKey } from '../../src/keys';
+import {
+  createStacksPrivateKey,
+  pubKeyfromPrivKey,
+  publicKeyToString,
+  createStacksPublicKey,
+  serializePublicKey,
+  deserializePublicKey,
+} from '../../src/keys';
 
 import { TransactionSigner } from '../../src/signer';
 
@@ -29,6 +52,7 @@ import * as BigNum from 'bn.js';
 import { enableFetchMocks } from 'jest-fetch-mock';
 import { BufferReader } from '../../src/bufferReader';
 import { standardPrincipalCV } from '../../src/clarity';
+import { length } from 'bn.js';
 
 enableFetchMocks();
 
@@ -56,7 +80,7 @@ test('STX token transfer transaction serialization and deserialization', () => {
   const fee = new BigNum(0);
   const pubKey = '03ef788b3830c00abe8f64f62dc32fc863bc0b2cafeb073b6c8e1c7657d9c2c3ab';
   const secretKey = 'edf9aee84d9b7abc145504dde6726c64f369d37ee34ded868fabd876c26570bc01';
-  const spendingCondition = new SingleSigSpendingCondition(addressHashMode, pubKey, nonce, fee);
+  const spendingCondition = createSingleSigSpendingCondition(addressHashMode, pubKey, nonce, fee);
   const authType = AuthType.Standard;
   const authorization = new StandardAuthorization(spendingCondition);
 
@@ -85,7 +109,9 @@ test('STX token transfer transaction serialization and deserialization', () => {
   expect(deserialized.version).toBe(transactionVersion);
   expect(deserialized.chainId).toBe(chainId);
   expect(deserialized.auth.authType).toBe(authType);
-  expect(deserialized.auth.spendingCondition!.addressHashMode).toBe(addressHashMode);
+  expect((deserialized.auth.spendingCondition! as SingleSigSpendingCondition).hashMode).toBe(
+    addressHashMode
+  );
   expect(deserialized.auth.spendingCondition!.nonce!.toNumber()).toBe(nonce.toNumber());
   expect(deserialized.auth.spendingCondition!.fee!.toNumber()).toBe(fee.toNumber());
   expect(deserialized.anchorMode).toBe(anchorMode);
@@ -122,7 +148,7 @@ test('STX token transfer transaction fee setting', () => {
   const fee = new BigNum(0);
   const pubKey = '03ef788b3830c00abe8f64f62dc32fc863bc0b2cafeb073b6c8e1c7657d9c2c3ab';
   const secretKey = 'edf9aee84d9b7abc145504dde6726c64f369d37ee34ded868fabd876c26570bc01';
-  const spendingCondition = new SingleSigSpendingCondition(addressHashMode, pubKey, nonce, fee);
+  const spendingCondition = createSingleSigSpendingCondition(addressHashMode, pubKey, nonce, fee);
   const authType = AuthType.Standard;
   const authorization = new StandardAuthorization(spendingCondition);
 
@@ -159,7 +185,9 @@ test('STX token transfer transaction fee setting', () => {
   expect(postSetFeeDeserialized.version).toBe(transactionVersion);
   expect(postSetFeeDeserialized.chainId).toBe(chainId);
   expect(postSetFeeDeserialized.auth.authType).toBe(authType);
-  expect(postSetFeeDeserialized.auth.spendingCondition!.addressHashMode).toBe(addressHashMode);
+  expect(
+    (postSetFeeDeserialized.auth.spendingCondition! as SingleSigSpendingCondition).hashMode
+  ).toBe(addressHashMode);
   expect(postSetFeeDeserialized.auth.spendingCondition!.nonce!.toNumber()).toBe(nonce.toNumber());
   expect(postSetFeeDeserialized.auth.spendingCondition!.fee!.toNumber()).toBe(setFee.toNumber());
   expect(postSetFeeDeserialized.anchorMode).toBe(anchorMode);
@@ -173,6 +201,74 @@ test('STX token transfer transaction fee setting', () => {
   expect(deserializedPostCondition.amount.toNumber()).toBe(0);
 
   const deserializedPayload = postSetFeeDeserialized.payload as TokenTransferPayload;
+  expect(deserializedPayload.recipient).toEqual(recipientCV);
+  expect(deserializedPayload.amount.toNumber()).toBe(amount.toNumber());
+});
+
+test('STX token transfer transaction multi-sig serialization and deserialization', () => {
+  const addressHashMode = AddressHashMode.SerializeP2SH;
+  const nonce = new BigNum(0);
+  const fee = new BigNum(0);
+
+  const privKeyStrings = [
+    '6d430bb91222408e7706c9001cfaeb91b08c2be6d5ac95779ab52c6b431950e001',
+    '2a584d899fed1d24e26b524f202763c8ab30260167429f157f1c119f550fa6af01',
+    'd5200dee706ee53ae98a03fba6cf4fdcc5084c30cfa9e1b3462dcdeaa3e0f1d201',
+  ];
+  const privKeys = privKeyStrings.map(createStacksPrivateKey);
+
+  const pubKeys = privKeyStrings.map(pubKeyfromPrivKey);
+  const pubKeyStrings = pubKeys.map(publicKeyToString);
+
+  const spendingCondition = createMultiSigSpendingCondition(
+    addressHashMode,
+    2,
+    pubKeyStrings,
+    nonce,
+    fee
+  );
+  const authType = AuthType.Standard;
+  const originAuth = new StandardAuthorization(spendingCondition);
+
+  const originAddress = originAuth.spendingCondition?.signer;
+
+  expect(originAddress).toEqual('a23ea89d6529ac48ac766f720e480beec7f19273');
+
+  const transactionVersion = TransactionVersion.Mainnet;
+  const chainId = DEFAULT_CHAIN_ID;
+
+  const anchorMode = AnchorMode.Any;
+
+  const address = 'SP3FGQ8Z7JY9BWYZ5WM53E0M9NK7WHJF0691NZ159';
+  const recipient = createStandardPrincipal(address);
+  const recipientCV = standardPrincipalCV(address);
+  const amount = new BigNum(2500000);
+  const memo = 'memo';
+
+  const payload = createTokenTransferPayload(recipientCV, amount, memo);
+
+  const transaction = new StacksTransaction(transactionVersion, originAuth, payload);
+
+  const signer = new TransactionSigner(transaction);
+  signer.signOrigin(privKeys[0]);
+  signer.signOrigin(privKeys[1]);
+  signer.appendOrigin(pubKeys[2]);
+
+  const serialized = transaction.serialize();
+  const deserialized = deserializeTransaction(new BufferReader(serialized));
+  expect(deserialized.version).toBe(transactionVersion);
+  expect(deserialized.chainId).toBe(chainId);
+  expect(deserialized.auth.authType).toBe(authType);
+  expect((deserialized.auth.spendingCondition! as MultiSigSpendingCondition).hashMode).toBe(
+    addressHashMode
+  );
+  expect(deserialized.auth.spendingCondition!.nonce!.toNumber()).toBe(nonce.toNumber());
+  expect(deserialized.auth.spendingCondition!.fee!.toNumber()).toBe(fee.toNumber());
+  expect(deserialized.anchorMode).toBe(anchorMode);
+  expect(deserialized.postConditionMode).toBe(PostConditionMode.Deny);
+  expect(deserialized.postConditions.values.length).toBe(0);
+
+  const deserializedPayload = deserialized.payload as TokenTransferPayload;
   expect(deserializedPayload.recipient).toEqual(recipientCV);
   expect(deserializedPayload.amount.toNumber()).toBe(amount.toNumber());
 });
